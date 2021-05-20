1de44379eae8e1c1e2c972761ea138984aab3c8f
priyanshi1208 <pri.shukla2208@gmail.com>
2021-05-20 18:39:55 +0530

    added new file

---
+++ LengthCalculation.java
@@ -0,0 +1,7 @@
+package com.linecomparison;
+
+public class LengthCalculation {
+	public double length(int x1, int y1, int x2, int y2) {
+		return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	}
+}
